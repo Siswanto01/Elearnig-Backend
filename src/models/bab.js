69bc6c40b9fdc536b9aa8eb6f8afad0e1bdeb4c1
Siswanto01 <sis98wanto@gmail.com>
2024-07-31 20:25:46 +0700

    Add Wanto

--- src/models/bab.js
+++ src/models/bab.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // define association here wanto
       bab.belongsTo(models.mata_pelajaran, {
         foreignKey: "mata_pelajaran_id",
         as: "mataPelajaran",
